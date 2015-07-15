3edb2cff4ff99ccf52655ba1dcc7ea332cc4ebd6
LONELY-WOLF <zvenayte@pisem.net>
2015-07-15 13:33:34 +0300

    Init

---
+++ Converters.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+namespace InterfaceSample
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, System.Type targetType, object parameter, string language)
+        {
+            if(value is bool?)
+            {
+                return ((bool?)value == true) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            if (value is bool)
+            {
+                return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        object IValueConverter.ConvertBack(object value, System.Type targetType, object parameter, string language)
+        {
+            if (value is Visibility)
+            {
+                if ((Visibility)value == Visibility.Visible)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using InterfaceSample.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace InterfaceSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        NavigationHelper navigationHelper;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+            navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (appBarToggleButton.IsChecked == true)
+            {
+                appBarToggleButton.IsChecked = false;
+                e.Handled = true;
+            }
+        }
+
+        private void listBoxItemMenu_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            appBarToggleButton.IsChecked = false;
+            if (sender == listBoxItemNews)
+            {
+                mainPivot.SelectedIndex = 0;
+            }
+            else if (sender == listBoxItemMail)
+            {
+                mainPivot.SelectedIndex = 1;
+            }
+            else if (sender == listBoxItemFav)
+            {
+                mainPivot.SelectedIndex = 2;
+            }
+            else if (sender == listBoxItemSettings)
+            {
+                
+            }
+            else if (sender == listBoxItemAbout)
+            {
+                this.Frame.Navigate(typeof(About));
+            }
+        }
+
+        private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        { }
+        private async void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        { }
+    }
+}
